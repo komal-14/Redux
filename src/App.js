cd7c6c5c297414d837e3093a3a4fd596b70f740f
komal-14 <komal.kakde@appxbuild.com>
2022-10-26 12:42:27 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
-import './App.css';
-import { Component } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { Fragment } from 'react';
+import Auth from './components/Auth';
+import Counter from './components/Counter';
+import Header from './components/Header';
 
 function App() {
-
-  const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const show = useSelector((state) => state.showCounter);
-
-  const incrementHandler = () => {
-
-    dispatch({ type: 'increment' });
-
-  };
-
-  const increaseHandler = () => {
-
-    dispatch({ type: 'increase' , amount: 10 });
-
-  };
-
-
-  const decrementHandler = () => {
-
-    dispatch({ type: 'decrement' });
-
-
-  };
-
-  const toggleCounterHandler = () => { 
-    dispatch({ type:'toggle' });
-  };
-
   return (
-    <main>
-      <div className="App">
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
 
-        <h1>Redux Counter</h1>
-
-        {show && <div>{counter} </div>}
-
-        <button onClick={incrementHandler}> Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-        <button onClick={increaseHandler}>Increase by 10</button>
-
-        {/* <button onClick={() => { dispatch({ type: "plusFive", payload: 5 }); }} style={{margin:10}}>plusFive</button> */}
-      {/* <button onClick={() => { dispatch({ type: "minusFive", payload: 5 }); }} style={{margin:10}}>minusFive</button> */}
- 
-        <div>
-          <button onClick={toggleCounterHandler}>Toggle Counter </button>
-        </div>
-      </div>
-    </main>
   );
-
-};
-
+}
 
 export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,61 @@
+import { useSelector , useDispatch } from 'react-redux';
+import { counterActions } from '../store/index';
+import classes from './Counter.module.css';
+
+const Counter = () => {
+
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+
+  const incrementHandler = () => {
+
+    dispatch(counterActions.increment());
+
+  };
+
+  const increaseHandler = () => {
+
+    dispatch(counterActions.increase(10));  // {type: SOME_UNIQUE_IDENTIFIER, payload:10}
+
+  };
+
+
+  const decrementHandler = () => {
+
+    dispatch(counterActions.decrement());
+
+
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+
+      <h1>Redux Counter</h1>
+
+      {show && <div className={classes.value}>{counter} </div>}
+
+      <div>
+
+        <button onClick={incrementHandler}> Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
+      </div>
+      {/* <button onClick={() => { dispatch({ type: "plusFive", payload: 5 }); }} style={{margin:10}}>plusFive</button> */}
+      {/* <button onClick={() => { dispatch({ type: "minusFive", payload: 5 }); }} style={{margin:10}}>minusFive</button> */}
+
+
+      <button onClick={toggleCounterHandler}>Toggle Counter </button>
+
+
+    </main>
+  );
+
+};
+
+
+export default Counter;
